22cf108bbb4d0035ebfaf4d65a1669cf95a2a3b3
arthurpaulmyers <54543515+arthurpaulmyers@users.noreply.github.com>
2019-11-20 07:07:12 -0500

    Navigating the API
    Preparing for SSUI

---
+++ scripts.js
@@ -0,0 +1,41 @@
+function pokeSubmit() {
+    var param = document.getElementById("pokeInput").value;
+    var pokeURL = "https://cors-anywhere.herokuapp.com/http://pokeapi.co/api/v1/pokemon/" + param;
+    //pokeAPI does not have an Access-Control-Allow-Origin value set
+    //so https://cors-anywhere.herokuapp.com/ is used to allow use of the api
+
+
+    $.getJSON(pokeURL, function(data) {
+        //console.log(data);
+        console.log(JSON.stringify(data, null, " "));
+        var pokeID = data.id;
+        var pokeName = data.name;
+        var pokeType1 = data.types[0].type.name;
+        if (data.types.length == 2) {
+            var pokeType2 = data.types[1].type.name;
+        }
+        else var pokeType2 = null;
+        var imageURI = data.sprites.front_default;
+
+        console.log("Number: ", pokeID);
+        console.log("Name: ", pokeName);
+        console.log("Type 1: ", pokeType1);
+        console.log("Type 2: ", pokeType2);
+        console.log("Image URI: ", imageURI);
+
+        var li="";
+        li += '<li><img src="' + imageURI + '">';
+        li += '<h1>#' + pokeID + ' ' + pokeName + '</h1>';
+        li += '<p>Type 1: ' + pokeType1 + '</p>';
+        
+        if (pokeType2 != null) {
+            li += '<p>Type 2: ' + pokeType2 + '</p>';
+        }
+        li += '</li>';
+        $("#pokeDetails").empty();
+
+        $("#pokeDetails").append(li).promise().done(function(){
+            $(this).listview("refresh");
+        });
+    });
+}

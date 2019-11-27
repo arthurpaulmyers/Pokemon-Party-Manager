2b12487bd45463614ebf912dfde340e5f05f3a94
arthurpaulmyers <54543515+arthurpaulmyers@users.noreply.github.com>
2019-11-26 20:36:01 -0500

    Created Pokemon class, added party and box logic
    Now, searching a Pokemon adds it to your party or box.

--- scripts.js
+++ scripts.js
@@ -1,10 +1,32 @@
+
+
+class Pokemon {
+    constructor(name, type1, type2, level, moveslist, slot, evol, move1, move2, move3, move4, sprite) {
+    this.name = name;
+    this.type1 = type1;
+    this.type2 = type2;
+    this.level = level;
+    this.moveslist = moveslist;
+    this.slot = slot;
+    this.evol = evol;
+    this.move1 = move1;
+    this.move2 = move2;
+    this.move3 = move3;
+    this.move4 = move4;
+    this.sprite = sprite;
+    }
+}
+
+var party = [];
+var box = [];
+
 function pokeSubmit() {
     var param = document.getElementById("pokeInput").value;
     var pokeURL = "https://cors-anywhere.herokuapp.com/http://pokeapi.co/api/v1/pokemon/" + param;
     //pokeAPI does not have an Access-Control-Allow-Origin value set
     //so https://cors-anywhere.herokuapp.com/ is used to allow use of the api
 
-
+//Get the data and push it to the console
     $.getJSON(pokeURL, function(data) {
         //console.log(data);
         console.log(JSON.stringify(data, null, " "));
@@ -42,19 +64,38 @@ function pokeSubmit() {
             }
         }
 
-        var li="";
-        li += '<li><img src="' + imageURI + '">';
-        li += '<h1>#' + pokeID + ' ' + pokeName + '</h1>';
-        li += '<p>Type 1: ' + pokeType1 + '</p>';
-        
-        if (pokeType2 != null) {
-            li += '<p>Type 2: ' + pokeType2 + '</p>';
+        var poke1 = new Pokemon(pokeName, pokeType1, pokeType2, '1', [], '1', 'yes', 'move1', 'move2', 'move3', 'move4', imageURI);
+        if (party.length < 6) {
+            party.push(poke1);
         }
-        li += '</li>';
-        $("#pokeDetails").empty();
+        else {
+            box.push(poke1);
+        }
 
-        $("#pokeDetails").append(li).promise().done(function(){
-            $(this).listview("refresh");
-        });
+        //making it appear on the screen
+        //var li="";
+        //li += '<li><img src="' + poke1.sprite + '">';
+        //li += '<h1>#' + pokeID + ' ' + poke1.name + '</h1>';
+        //li += '<p>Type 1: ' + poke1.type1 + '</p>';
+        
+        //if (pokeType2 != null) {
+        //    li += '<p>Type 2: ' + poke1.type2 + '</p>';
+        //}
+        //li += '<p>Move 1: ' + poke1.move1 + '</p>';
+        //li += '<p>Move 2: ' + poke1.move2 + '</p>';
+        //li += '<p>Move 3: ' + poke1.move3 + '</p>';
+        //li += '<p>Move 4: ' + poke1.move4 + '</p>';
+        //li += '<p>Evolution Method: ' + poke1.evol + '</p>';
+        //li += '<p>Slot: ' + poke1.slot + '</p>';
+        //li += '</li>';
+
+        console.log(party);
+        console.log(box);
+
+        //$("#pokeDetails").empty();
+
+        //$("#pokeDetails").append(li).promise().done(function(){
+        //    $(this).listview("refresh");
+        //});
     });
 }
